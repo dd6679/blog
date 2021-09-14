95e41fa31a2fac8c9ebeffea1f7ce893964c4c9d
kimnakyung <35446851+dd6679@users.noreply.github.com>
2021-09-14 17:25:15 +0900

    db 작성

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Member(models.Model):
+    id = models.CharField(primary_key=True, max_length=32)
+    password = models.CharField(max_length=32)
+    nickname = models.CharField(max_length=10)
+    email = models.CharField(max_length=128)
+
+
+class Posts(models.Model):
+    title = models.CharField(primary_key=True, max_length=32)
+    post_text = models.TextField()
+    public = models.BooleanField()
+    date = models.DateTimeField(auto_now_add=True)
+    #image = models.ImageField()
+    #video = models.CharField(max_length=128, blank=True, null=True) n개..
+
+
+class Comment(models.Model):
+    post_title = models.ForeignKey('Posts', db_column='title', on_delete=models.CASCADE, null=True)
+    comment_text = models.TextField(max_length=128)
+    public = models.BooleanField()
+    date = models.DateTimeField(auto_now_add=True)
+
+
+
+
--- config/urls.py
+++ config/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog import views
+from django.urls import include, path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', views.index),
+    path('blog/', include('blog.urls')),
 ]
